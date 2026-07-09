503d0c5619a974756cbb9a36a80ecf1833377433
weijinjie9527 <weijinjie9527@163.com>
2026-07-10 01:08:46 +0800

    Update noBiLSTM.py

--- noBiLSTM.py
+++ noBiLSTM.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import importlib.util
-sys.path.append('../input')
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -11,8 +10,8 @@ import csv
 from tqdm import tqdm
 from sklearn.metrics import accuracy_score, roc_auc_score, precision_recall_curve, auc
 from torch.utils.data import random_split
-from wjjtfbs.Datasets.DataReader import Datasets
-from wjjtfbs.models.noBiLSTM import CLAModel
+from Datasets.DataReader import Datasets
+from models.noBiLSTM import CLAModel
 
 
 class Constructor:
